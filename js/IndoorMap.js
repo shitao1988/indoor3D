c76d54fdfa5a6a3f4bb8134e9c6f26f44f2d6605
shitao <shitao19_88@163.com>
2016-12-13 17:24:40 +0800

    update

--- js/IndoorMap.js
+++ js/IndoorMap.js
@@ -1,6 +1,71 @@
 /**
  * Created by gaimeng on 14/12/27.
  */
+
+WktParser = function() {}
+
+/**将wkt转换为坐标数组，例如：
+//POLYGON ((116.993225097656 36.892822265625, 
+//    116.993225097656 36.8926391601563, 
+//    116.993408203125 36.8926391601563, 
+//    116.993408203125 36.892822265625, 
+//    116.993225097656 36.892822265625))
+//转换为：[[36.892822265625, 116.993225097656], 
+//    [36.8926391601563, 116.993225097656], 
+//    [36.8926391601563, 116.993408203125], 
+//    [36.892822265625, 116.993408203125], 
+//    [36.892822265625, 116.993225097656]]
+ */
+WktParser.wkt2Array = function(wkt) {
+    if (!wkt) {
+        return;
+    }
+
+    wkt = wkt.toLowerCase().replace(/\s+/, " "); //将多空格替换为单空格
+    //var reg = /[\s,\(]([\b\.]+)\s([\b\.])+[\s,\)]/;    //匹配坐标组
+    var reg = /([\d\.]+)\s([\d\.])+\s([\d\.])+/g; //匹配三维坐标组
+    wkt = wkt.replace(reg, "[$2, $1, $3]"); //将116.993225097656 36.892822265625 1000转变为[36.892822265625, 116.993225097656, 1000]
+    reg = /([\d\.]+)\s+([\d\.]+)/g; //匹配二维坐标组
+    wkt = wkt.replace(reg, "[$2, $1]"); //将116.993225097656 36.892822265625转变为[36.892822265625, 116.993225097656]
+
+    var result = {};
+
+    if (wkt.indexOf("multipoint") >= 0) {
+        result.type = "MultiPoint";
+    } else if (wkt.indexOf("point") >= 0) {
+        result.type = "Point";
+    } else if (wkt.indexOf("multipolyline") >= 0) {
+        result.type = "MultiPolyline";
+    } else if (wkt.indexOf("polyline") >= 0) {
+        result.type = "Polyline";
+    } else if (wkt.indexOf("multipolygon") >= 0) {
+        result.type = "MultiPolygon";
+    } else if (wkt.indexOf("polygon") >= 0) {
+        result.type = "Polygon";
+    }
+
+    if (result.type) {
+        result.coords = WktParser._getCoords(wkt.substring(result.type.length));
+
+        //if (result.coords.length == 1) {
+        //    result.coords = result.coords[0];
+        //}
+    } else {
+        result = null;
+    }
+
+    return result;
+}
+String.prototype.trim = function() {　　 return this.replace(/(^\s*)|(\s*$)/g, "");　　 }
+
+WktParser._getCoords = function(wkt) {
+    wkt = wkt.replace(/\(/g, "[");
+    wkt = wkt.replace(/\)/g, "]");
+    wkt = wkt.trim();
+
+    return eval('(' + wkt + ')');
+}
+
 var System = {};
 var js = document.scripts;
 js = js[js.length - 1].src.substring(0, js[js.length - 1].src.lastIndexOf("/"));
@@ -982,7 +1047,7 @@ function ParseModel(json, is3d, theme, sceneSize) {
     }
 
     function getWKTPoints(wktString) {
-        var parseResult = Z.WktParser.wkt2Array(wktString),
+        var parseResult = WktParser.wkt2Array(wktString),
             pointArray = [];
         if (parseResult.type === "MultiPolygon") {
             pointArray = parseResult.coords;
